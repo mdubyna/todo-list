ef86839ce4d6012fe5a263b6923367f89ea1ab01
Mykola <mykola22dubyna@gmail.com>
2024-03-11 14:56:51 +0200

    Add forms to app todo
    -Add form for task-create and task-update

---
+++ todo/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from todo.models import Tag, Task
+
+
+class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+        widgets = {
+            "deadline": forms.DateInput(attrs={"type": "datetime-local"})
+        }
