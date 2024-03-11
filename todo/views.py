15bc2480dd8fb6a11bde37d8449743af27c2c5d9
Mykola <mykola22dubyna@gmail.com>
2024-03-11 14:55:49 +0200

    Add views to app todo view
    -Add view for task-list
    -Add view for task-create
    -Add view for task-update
    -Add view for task-delete
    -Add view for tag-list
    -Add view for tag-create
    -Add view for tag-update
    -Add view for tag-delete

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo.forms import TaskForm
+from todo.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
+    ordering = ["done", "-created_at"]
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+def done_task(request: HttpRequest, pk: int) -> HttpResponse:
+    task = Task.objects.get(pk=pk)
+    if task.done:
+        task.done = False
+    else:
+        task.done = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("todo:task-list"))
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+    fields = "__all__"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+    fields = "__all__"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
